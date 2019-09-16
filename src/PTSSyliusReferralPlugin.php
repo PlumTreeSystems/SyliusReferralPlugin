bc6063df92d74d6e8df21346a15a3c277aa3fbe7
Mykolas <mykolasvitkus@gmail.com>
2019-09-16 14:59:49 +0300

    plugin init

---
+++ src/PTSSyliusReferralPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PTS\SyliusReferralPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class PTSSyliusReferralPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
